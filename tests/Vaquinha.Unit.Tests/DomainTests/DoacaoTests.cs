0450d28fb47d0f185b922a954d4d30d3adb81b3d
Gabriel <bielgag14@gmail.com>
2021-02-01 23:49:30 -0300

    feito teste de taxa extra

--- tests/Vaquinha.Unit.Tests/DomainTests/DoacaoTests.cs
+++ tests/Vaquinha.Unit.Tests/DomainTests/DoacaoTests.cs
@@ -38,6 +38,23 @@ namespace Vaquinha.Unit.Tests.DomainTests
         }
 
         [Fact]
+        [Trait("Doacao", "UsuarioAceitaPagarComTaxaDoacao_CorretamentePreenchidos_DoacaoValida")]
+        public void UsuarioAceitaPagarComTaxaDoacao_CorretamentePreenchidos_DoacaoValida()
+        {           
+            // Arrange
+            var doacao = _doacaoFixture.DoacaoValida(false, 5, false, true);
+            doacao.AdicionarEnderecoCobranca(_enderecoFixture.EnderecoValido());
+            doacao.AdicionarFormaPagamento(_cartaoCreditoFixture.CartaoCreditoValido());
+
+            // Act
+            var valido = doacao.Valido();
+
+            // Assert
+            doacao.Valor.Should.Be(6, because: "Valor com taxa de 20%");
+        }
+
+
+        [Fact]
         [Trait("Doacao", "Doacao_DadosPessoaisInvalidos_DoacaoInvalida")]
         public void Doacao_DadosPessoaisInvalidos_DoacaoInvalida()
         {
